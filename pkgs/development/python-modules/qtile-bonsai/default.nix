64a35476a43e11bbf34004bc398f413c11b0e0a3
Gurjaka <esartia.gurika@gmail.com>
2024-11-17 16:20:42 +0400

    qtile-bonsai: init at 0.4.0
    Update pkgs/development/python-modules/qtile-bonsai/default.nix
    
    Co-authored-by: Arne Keller <2012gdwu+github@posteo.de>

---
+++ pkgs/development/python-modules/qtile-bonsai/default.nix
@@ -0,0 +1,65 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  cairocffi,
+  cffi,
+  strenum,
+  psutil,
+  xcffib,
+  pdm-backend,
+  pyside6,
+  pyvirtualdisplay,
+  qtile,
+}:
+
+buildPythonPackage rec {
+  pname = "qtile-bonsai";
+  version = "0.4.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "aravinda0";
+    repo = "qtile-bonsai";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-IWy/YEVdZc+UgIKl75ZpOkOIvpS5hCX0ihQenUOuJHo=";
+  };
+
+  build-system = [
+    pdm-backend
+  ];
+
+  dependencies = [
+    strenum
+    psutil
+    pyside6
+    pyvirtualdisplay
+    (cairocffi.override { withXcffib = true; })
+    cffi
+    xcffib
+    qtile
+  ];
+
+  preCheck = ''
+    export HOME=$(mktemp -d)
+  '';
+
+  disabledTestPaths = [
+    # Needs a running DBUS
+    "tests/integration/test_layout.py"
+    "tests/integration/test_widget.py"
+  ];
+
+  pythonImportCheck = [ "qtile_bonsai" ];
+
+  meta = {
+    changelog = "https://github.com/aravinda0/qtile-bonsai/releases/tag/${version}";
+    homepage = "https://github.com/aravinda0/qtile-bonsai";
+    description = "Flexible layout for the qtile tiling window manager";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      gurjaka
+      sigmanificient
+    ];
+  };
+}
